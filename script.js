9c7d06583e738d110c66e1046cc24d2616b46f32
Kaylin Lapan <kaylinlapan@gmail.com>
2022-04-01 18:01:18 -0400

    added gantt chart from google charts

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Donut chart*/
 const data = {
     labels: [
       'Facility Requests',
@@ -30,7 +31,7 @@ const data = {
       config
     );
 
-    
+/*Line Chart*/    
 const labels = [
     '2018',
     '2019',
@@ -58,3 +59,80 @@ const lineChart = new Chart(
       document.getElementById('lineChart'),
       configTwo
     );
+/*Gnatt Chart*/
+
+    google.charts.load('current', {'packages':['gantt']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var gdata = new google.visualization.DataTable();
+      gdata.addColumn('string', 'Task ID');
+      gdata.addColumn('string', 'Task Name', 'color: #b87333');
+      gdata.addColumn('string', 'Resource', 'color: #b87333');
+      gdata.addColumn('date', 'Start Date', 'color: #e5e4e2');
+      gdata.addColumn('date', 'End Date', 'color: #e5e4e2');
+      gdata.addColumn('number', 'Duration','color: #e5e4e2');
+      gdata.addColumn('number', 'Percent Complete','color: #e5e4e2');
+      gdata.addColumn('string', 'Dependencies','color: #e5e4e2');
+
+      gdata.addRows([
+        ['2014Spring', 'Spring 2014', 'spring',
+         new Date(2014, 2, 22), new Date(2014, 5, 20), null, 100, null],
+        ['2014Summer', 'Summer 2014', 'summer',
+         new Date(2014, 5, 21), new Date(2014, 8, 20), null, 100, null],
+        ['2014Autumn', 'Autumn 2014', 'autumn',
+         new Date(2014, 8, 21), new Date(2014, 11, 20), null, 100, null],
+        ['2014Winter', 'Winter 2014', 'winter',
+         new Date(2014, 11, 21), new Date(2015, 2, 21), null, 100, null],
+        ['2015Spring', 'Spring 2015', 'spring',
+         new Date(2015, 2, 22), new Date(2015, 5, 20), null, 50, null],
+        ['2015Summer', 'Summer 2015', 'summer',
+         new Date(2015, 5, 21), new Date(2015, 8, 20), null, 0, null],
+        ['2015Autumn', 'Autumn 2015', 'autumn',
+         new Date(2015, 8, 21), new Date(2015, 11, 20), null, 0, null],
+        ['2015Winter', 'Winter 2015', 'winter',
+         new Date(2015, 11, 21), new Date(2016, 2, 21), null, 0, null],
+        ['Football', 'Football Season', 'sports',
+         new Date(2014, 8, 4), new Date(2015, 1, 1), null, 100, null],
+        ['Baseball', 'Baseball Season', 'sports',
+         new Date(2015, 2, 31), new Date(2015, 9, 20), null, 14, null],
+        ['Basketball', 'Basketball Season', 'sports',
+         new Date(2014, 9, 28), new Date(2015, 5, 20), null, 86, null],
+        ['Hockey', 'Hockey Season', 'sports',
+         new Date(2014, 9, 8), new Date(2015, 5, 21), null, 89, null]
+      ]);
+
+      var options = {
+        height: 400,
+        gantt: {
+          palette: [
+  {
+    "color": "#777",
+    "dark": "#555",
+    "light": "#999"
+  },
+    
+],
+          trackHeight: 30,
+          innerGridTrack: {fill: '#fff'},
+          innerGridDarkTrack: {fill: '#eee'},
+          labelStyle: {
+           fontName: 'Source Sans Pro',
+           fontSize: 14,
+           color: '#000',
+          },
+        },
+
+        backgroundColor:{
+          fill: 'white',
+        },
+      };
+console.log(options);
+console.log(gdata);
+
+
+      var chart = new google.visualization.Gantt(document.getElementById('chart_div'));
+
+      chart.draw(gdata, options);
+    }
